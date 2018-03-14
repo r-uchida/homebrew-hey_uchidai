b8a29b716bcfe6611345352c3e9d89bb52ef19d4
Daisuke Uchida <uchida@rolan.co.jp>
2018-03-14 17:04:13 +0900

    first commit

---
+++ hey_uchidai.rb
@@ -0,0 +1,9 @@
+class HeyUchidai < Formula
+  desc ""
+  homepage ""
+  url "https://github.com/r-uchida/hey_uchidai/releases/download/release%2F1.0.0/hey_uchidai"
+  sha256 "ffd0afe85742dcb7349d1495fb1fa8c25d749931d466cebe663ecb30ed065c12"
+  def install
+    bin.install "hey_uchidai"
+  end
+end
